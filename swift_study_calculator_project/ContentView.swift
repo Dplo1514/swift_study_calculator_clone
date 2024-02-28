20984078b28ee72523649c0a3c68af839f4df762
posicube-InHyuk <ihlim@posicube.com>
2024-02-29 00:14:01 +0900

    Initial Commit

---
+++ swift_study_calculator_project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_study_calculator_project
+//
+//  Created by PLO on 2024/02/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_study_calculator_project/swift_study_calculator_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_study_calculator_projectApp.swift
+//  swift_study_calculator_project
+//
+//  Created by PLO on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct swift_study_calculator_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
